18936224e6d1d2dd5625f69ab49e1d8b7a5e0647
Johan Guldmyr <johan@meetiqm.com>
2024-01-03 12:07:43 +0200

    disable auto-formatting yaml files

--- lua/plugs/formatting.lua
+++ lua/plugs/formatting.lua
@@ -14,7 +14,7 @@ return {
                 markdown = { 'markdown-toc', 'mdformat' },
                 python = { 'isort', 'black' }, -- replaces pylsp formatting (with autopep8/yapf)
                 tex = { 'latexindent' },
-                yaml = { 'yamlfix' },
+                -- yaml = { 'yamlfix' }, -- Changes too much in ours
                 ['*'] = { 'codespell' },
                 ['_'] = { 'trim_whitespace', 'squeeze_blanks' },
             },
